fe95ebb615efa93a1db8889804538b3cf427a1b6
konan-fabrice <kouameaimefabrice@gmail.com>
2023-12-02 09:44:29 +0000

    test first commit online

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -109,21 +109,21 @@
                             <div class="col-xl-4 d-flex align-items-stretch">
                                 <div class="mt-4 icon-box mt-xl-0">
                                     <i class="bx bx-receipt"></i>
-                                    <h4>Corporis voluptates sit</h4>
+                                    <h4>Notre objectif</h4>
                                     <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
                                 </div>
                             </div>
                             <div class="col-xl-4 d-flex align-items-stretch">
                                 <div class="mt-4 icon-box mt-xl-0">
                                     <i class="bx bx-cube-alt"></i>
-                                    <h4>Ullamco laboris ladore pan</h4>
+                                    <h4>Notre mission</h4>
                                     <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
                                 </div>
                             </div>
                             <div class="col-xl-4 d-flex align-items-stretch">
                                 <div class="mt-4 icon-box mt-xl-0">
                                     <i class="bx bx-images"></i>
-                                    <h4>Labore consequatur</h4>
+                                    <h4>Notre engagement</h4>
                                     <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
                                 </div>
                             </div>
